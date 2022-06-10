55d39b349e130263cc95036085ee7a0f00a487be
benndora1 <benndora2@gmail.com>
2022-06-10 16:04:52 +0300

    db

---
+++ database_service.py
@@ -0,0 +1,11 @@
+import harperdb
+
+url = 'https://cloud-1-ben.harperdbcloud.com'
+user_name = 'ben'
+password = 'Ben@19949064'
+
+db = harperdb.HarperDB(
+    url=url,
+    user_name = user_name,
+    password = password,
+)
